1f0d784f8856a1b6f1d9c5089d81d77d5b218674
Dmitry Kalinin <kalinin.mitko@gmail.com>
2024-01-05 15:09:44 +0200

    Update __init__.py

--- logs/models/__init__.py
+++ logs/models/__init__.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models import Model
+from django.utils.translation import gettext_lazy as _
 from logs.models.fields import (
     CreatingUserField,
     CreatingSessionKeyField,
@@ -15,17 +16,17 @@ class AuthStampedModel(Model):
     """
 
     created_by = CreatingUserField(
-        verbose_name="created by",
-        related_name="created_%(app_label)s_%(class)s_set",
+        verbose_name=_("created by"),
+        related_name=_("created_%(app_label)s_%(class)s_set"),
         on_delete=models.SET_NULL,
     )
     created_with_session_key = CreatingSessionKeyField(_("created with session key"))
     modified_by = LastUserField(
-        verbose_name="modified by",
-        related_name="modified_%(app_label)s_%(class)s_set",
+        verbose_name=_("modified by"),
+        related_name=_("modified_%(app_label)s_%(class)s_set"),
         on_delete=models.SET_NULL,
     )
-    modified_with_session_key = LastSessionKeyField("modified with session key")
+    modified_with_session_key = LastSessionKeyField(_("modified with session key"))
 
     class Meta:
         abstract = True
